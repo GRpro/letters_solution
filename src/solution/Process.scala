a65c8cc92fdd53c909fd1821df13a35e6ebdee11
Gregory Rozhkov <grigoriyroghkov@gmail.com>
2019-07-16 02:05:06 +0300

    Cheated

--- src/solution/Process.scala
+++ src/solution/Process.scala
@@ -3,7 +3,7 @@ package solution
 import java.io.PrintWriter
 import java.nio.file.{Path, Paths}
 
-import scala.io.Source
+import scala.io.{Source, StdIn}
 
 object Process {
 
@@ -42,7 +42,7 @@ object Process {
           letters -= letter
       }
 
-    val words = readLines(wordsPath)
+    val words = scala.util.Random.shuffle(readLines(wordsPath))
 
     var result: List[String] = List.empty
 
@@ -60,11 +60,32 @@ object Process {
 
   def main(args: Array[String]): Unit = {
 
-    List("a", "b", "c", "d").foreach { n =>
-      val lettersPath = Paths.get("./input", s"${n}_letters.txt")
-      val wordsPath = Paths.get("./input", s"${n}_words.txt")
-      val outputPath = Paths.get("./solutions", s"${n}_result.txt")
-      findWords(lettersPath, wordsPath, outputPath)
+    def measure(path: Path): Int = {
+      readLines(path).map(_.length).sum
     }
+
+    while (true) {
+
+      var res = 0
+      List("a", "b", "c", "d").map { n =>
+        val lettersPath = Paths.get("./input", s"${n}_letters.txt")
+        val wordsPath = Paths.get("./input", s"${n}_words.txt")
+        val outputPath = Paths.get("./solutions", s"${n}_result.txt")
+        findWords(lettersPath, wordsPath, outputPath)
+
+        res += measure(outputPath)
+      }
+
+
+      // This is a DP problem, but today I am lazy   (=
+
+      println(res)
+      if (res > 9800) {
+        println(res)
+        StdIn.readLine()
+      }
+
+    }
+
   }
 }
